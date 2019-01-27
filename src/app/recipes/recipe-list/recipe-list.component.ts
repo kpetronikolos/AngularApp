d1052ad12612f19c261941f54c9ee22aa5dce52b
konstantinos <kpetronikolos@gmail.com>
2019-01-27 17:45:46 +0200

    Outputting a test List of Recipes.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test Recipe', 'Test Description', 
+    'https://vignette.wikia.nocookie.net/injusticegodsamongus/images/7/74/Kirby_%28Air_Ride%29.jpg/revision/latest?cb=20130703215944'),
+    new Recipe('My Recipe', 'My Description', 'https://vignette.wikia.nocookie.net/injusticegodsamongus/images/7/74/Kirby_%28Air_Ride%29.jpg/revision/latest?cb=20130703215944')
+  ];
 
   constructor() { }
 
